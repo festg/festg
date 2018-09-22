c304d22d1ecd5ba4d17729e08582c6f970cdc8b8
elipmoc <relipmoc101@gmail.com>
2018-09-23 04:47:28 +0900

    ゲームBGMをランダムで鳴らすように

---
+++ Assets/Scripts/Game/GameMusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusic : MonoBehaviour {
+
+    [SerializeField]
+    List<AudioClip> audioClips;
+
+    [SerializeField]
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        if (audioClips.Count != 0)
+        {
+            audioSource.clip = audioClips[(int)Mathf.Floor(Random.value * audioClips.Count)];
+            audioSource.Play();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
