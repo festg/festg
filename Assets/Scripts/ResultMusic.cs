f5c1ce5eebbaee00334c7985c851879ec393742b
elipmoc <relipmoc101@gmail.com>
2018-09-23 05:01:33 +0900

    ランク別BGM切り替え実装

---
+++ Assets/Scripts/ResultMusic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultMusic : MonoBehaviour {
+    [SerializeField]
+    AudioSource audioSource;
+
+
+    [SerializeField]
+    AudioClip RankS;
+
+    [SerializeField]
+    AudioClip RankA;
+
+    [SerializeField]
+    AudioClip RankBC;
+
+    [SerializeField]
+    AudioClip RankD;
+
+    // Use this for initialization
+    void Start (){
+        switch(GameObject.Find("Rank manegement").GetComponent<RankManagement>().GetRank())
+        {
+            case RankKind.S:
+                audioSource.clip = RankS;
+                break;
+            case RankKind.A:
+                audioSource.clip = RankA;
+                break;
+            case RankKind.B:
+            case RankKind.C:
+                audioSource.clip = RankBC;
+                break;
+            case RankKind.D:
+                audioSource.clip = RankD;
+                break;
+            default:
+                return;
+        }
+        audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
