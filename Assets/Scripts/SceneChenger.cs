b2e4c7e00d81b89abc9efe176ec777b2fb16610a
elipmoc <relipmoc101@gmail.com>
2018-09-22 17:33:02 +0900

    SceneChangerの修正

--- Assets/Scripts/SceneChenger.cs
+++ Assets/Scripts/SceneChenger.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,16 +9,13 @@ public string sceneName;
 
 	// Use this for initialization
 	void Start () {
-
-        if (Input.GetKeyDown(KeyCode.Return))
-            Debug.Log("Return key was pressed.");
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyUp(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Return key was released");
             SceneManager.LoadScene(sceneName);
