f7db611a54d21d444a1445cb0871618999ab7405
elipmoc <relipmoc101@gmail.com>
2018-09-22 14:47:30 +0900

    json読み込めるようにした

---
+++ Assets/Scripts/Game/QuestionGetter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class QuestionGetter {
+    public static  QuestionsData Deserialize(string yamlName)
+    {
+        StreamReader sr = new StreamReader(yamlName);
+        string text = sr.ReadToEnd();
+        var data=JsonUtility.FromJson<QuestionsData>(text);
+        return data;
+    }
+}
