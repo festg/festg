8bf4eed5aad7123e1c9aefc16453eb7f7725ae95
unknown <shige-fairystory.748@au.comgit config --global user.name w-shigematsu>
2018-09-22 17:39:16 +0900

    当たり判定、プレイヤー速度向上

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -7,6 +7,24 @@ public class Player : MonoBehaviour
     //速度
     public float speed = 5;
 
+    //移動制限
+    private Vector2 player_pos;
+
+        void Clamp()
+        {
+            //プレイヤーの位置を取得
+            player_pos = transform.position;       
+            
+            //x位置が常に範囲内か監視
+            player_pos.x = Mathf.Clamp(player_pos.x, -590, 590);
+            //y位置が常に範囲内か監視
+            player_pos.y = Mathf.Clamp(player_pos.y, -475, 475);
+            //範囲内であれば常にその位置にそのまま入る
+            transform.position = new Vector2(player_pos.x, player_pos.y);
+
+        }
+
+
     //Use this for initialization
     private void Start()
     {
@@ -20,24 +38,27 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
 
-            transform.Translate(0.0f, 3f, 0.0f);
+            transform.Translate(0.0f, 8f, 0.0f);
 
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0.0f, -3f, 0.0f);
+            transform.Translate(0.0f, -8f, 0.0f);
 
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-3f, 0.0f, 0.0f);
+            transform.Translate(-8f, 0.0f, 0.0f);
 
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(3f, 0.0f, 0.0f);
+            transform.Translate(8f, 0.0f, 0.0f);
 
         }
+
+        Clamp();
+
     }
 
 }
