4e5ef9e62a8ee29a119e47d13a05b7b14df48843
unknown <shige-fairystory.748@au.comgit config --global user.name w-shigematsu>
2018-09-22 14:46:08 +0900

    プレイヤーの移動

---
+++ Assets/PlayerControlScript.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+//速度
+var SPEED: Vector2 = Vector2(0.05f, 0.05f);
+
+//Use this for initialization
+function Start() {
+
+}
+
+//Update is called once per frame
+function Update() {
+
+    //移動速度
+    Move();
+
+}
+
+//移動関数
+function Move() {
+
+    //Aキーを押し続けていたら
+    if (Input.GetKey("A")) {
+
+        //代入したPositionに対して加算減算を行う
+        transform.position.x -= SPEED.x;
+
+    } else if (Input.GetKey("D")) {       //Dキーを押し続けていたら
+
+        //代入したPositionに対して加算減算を行う
+        transform.position.x += SPEED.x;
+
+    } else if (Input.GetKey("W")) {     //Wキーを押し続けていたら
+
+        //代入したPositionに対して加算減算を行う
+        transform.position.y += SPEED.y;
+
+    } else if (Input.GetKey("S")) {     //Sキーを押し続けていたら
+
+        //代入したPositionに対して加算減算を行う
+        transform.position.y -= SPEED.y;
+
+    }
+
+}
---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    //速度
+    public float speed = 5;
+
+    //Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            transform.Translate(0.0f, 3f, 0.0f);
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0.0f, -3f, 0.0f);
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-3f, 0.0f, 0.0f);
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(3f, 0.0f, 0.0f);
+
+        }
+    }
+
+}
